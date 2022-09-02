cbb8d552ed576896b18131c2d6f4f956ca1f8ace
hana604 <112719704+hana604@users.noreply.github.com>
2022-09-02 19:08:54 +0200

    modif

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -8,10 +8,8 @@ const About = () => (
   <Main meta={<Meta title="Lorem ipsum" description="Lorem ipsum" />}>
     <Content>
       <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga
-        recusandae quidem. Quaerat molestiae blanditiis doloremque possimus
-        labore voluptatibus distinctio recusandae autem esse explicabo molestias
-        officia placeat, accusamus aut saepe.
+       Welcome to our website! 
+       Here you will find cheap wonderbox! 
       </p>
       <p>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga
